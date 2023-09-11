aa7edc3e970eb10e89fb7ed857773e430733535b
mlwelker <nutria.watts.0g@icloud.com>
2023-09-11 17:58:53 -0500

    with conditional filter parameters

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -20,16 +20,24 @@ struct ContentView: View {
         let beginImage = CIImage(image: inputImage)
         
         let context = CIContext()
-//        let currentFilter = CIFilter.sepiaTone()
-//        let currentFilter = CIFilter.pixellate()
-//        let currentFilter = CIFilter.crystallize()
         let currentFilter = CIFilter.twirlDistortion()
         currentFilter.inputImage = beginImage
-//        currentFilter.intensity = 1
-//        currentFilter.scale = 25
-//        currentFilter.radius = 50
-        currentFilter.radius = 500
-        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
+        
+        let amount = 1.0
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
+        }
+        
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
+        }
+        
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
+        }
+        
         
         guard let outputImage = currentFilter.outputImage else { return }
         
