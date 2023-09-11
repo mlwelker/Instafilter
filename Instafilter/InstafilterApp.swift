6bd11ede8a1a87cbe392e9c3d5e616ee07bac4df
mlwelker <nutria.watts.0g@icloud.com>
2023-09-11 09:18:30 -0500

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Michael Welker on 2023-09-11.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
