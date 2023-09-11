f17540ff33af07d37626e50dc0bd593f710e4e23
mlwelker <nutria.watts.0g@icloud.com>
2023-09-11 09:44:18 -0500

    confirmationDialog

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -2,25 +2,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount = 0.0
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
     
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .blur(radius: blurAmount)
-            
-            Slider(value: $blurAmount, in: 0...20)
-            
-            Button("Random Blur") {
-                blurAmount = Double.random(in: 0...20)
+        Text("Hello, world!")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                showingConfirmation = true
             }
-        }
-        .padding()
-        .onChange(of: blurAmount) { newValue in
-            print("New value: \(newValue)")
-        }
-        // .onChange can be attached to anything in the view hierarchy
-        // recommended to put it on the element that is changing the property
+            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+                Button("Red") { backgroundColor = .red }
+                Button("Green") { backgroundColor = .green }
+                Button("Blue") { backgroundColor = .blue }
+                Button("Cancel", role: .cancel) { }
+            } message: {
+                Text("Select a new color")
+            }
     }
 }
 
