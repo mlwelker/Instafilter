53491f9c4012b51a547daf5cfdfbb8241373cff4
mlwelker <nutria.watts.0g@icloud.com>
2023-09-11 14:54:02 -0500

    vanilla SwiftUI Image view

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -2,24 +2,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
     
     var body: some View {
-        Text("Hello, world!")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                showingConfirmation = true
-            }
-            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                Button("Red") { backgroundColor = .red }
-                Button("Green") { backgroundColor = .green }
-                Button("Blue") { backgroundColor = .blue }
-                Button("Cancel", role: .cancel) { }
-            } message: {
-                Text("Select a new color")
-            }
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        image = Image("Example")
     }
 }
 
