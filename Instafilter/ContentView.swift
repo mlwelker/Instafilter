1131286f40dbb42827d45dd0e9d1ff5ef7f02d5f
mlwelker <nutria.watts.0g@icloud.com>
2023-09-12 18:15:34 -0500

    reset for part 4

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -2,36 +2,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var inputImage: UIImage?
-    @State private var showingImagePicker = false
-    
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select Image") {
-                showingImagePicker = true
-            }
-            
-            Button("Save Image") {
-                guard let inputImage else { return }
-                
-                let imageSaver = ImageSaver()
-                imageSaver.writeToPhotoAlbum(image: inputImage)
-            }
-        }
-        .sheet(isPresented: $showingImagePicker) {
-            ImagePicker(image: $inputImage)
-        }
-        .onChange(of: inputImage) { _ in loadImage() }
-    }
-    
-    func loadImage() {
-        guard let inputImage else { return }
-        image = Image(uiImage: inputImage)
+        Text("hello world")
     }
 }
 
