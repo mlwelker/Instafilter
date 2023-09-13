96c56d7ec280eafa5c816568e5c7649eaacc2c9e
mlwelker <nutria.watts.0g@icloud.com>
2023-09-12 20:48:08 -0500

    Importing an image into SwiftUI using PHPickerViewController

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,6 +5,9 @@ struct ContentView: View {
     @State private var image: Image?
     @State private var filterIntensity = 0.5
     
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -21,7 +24,7 @@ struct ContentView: View {
                         .scaledToFit()
                 }
                 .onTapGesture {
-                    // TODO select an image
+                    showingImagePicker = true
                 }
                 
                 HStack {
@@ -43,7 +46,16 @@ struct ContentView: View {
             }
             .padding([.horizontal, .bottom])
             .navigationTitle("Instafilter")
+            .onChange(of: inputImage) { _ in loadImage() }
+            .sheet(isPresented: $showingImagePicker) {
+                ImagePicker(image: $inputImage)
+            }
         }
+    }
+    
+    func loadImage() {
+        guard let inputImage else { return }
+        image = Image(uiImage: inputImage)
     }
     
     func save() {
